bac2af435de22892f781dc8990682bf2fc51b999
Mohammad <moa@dragonfli.es>
2015-08-11 19:08:20 -0400

    Dropdown and select function and event added

--- colormap-chooser.js
+++ colormap-chooser.js
@@ -1,5 +1,6 @@
 /*
  * A widget for choosing colorbrewer colormaps
+ * Requires D3, colorbrewer, jQuery and Bootstrap
  * https://github.com/auxiliary/colormap-chooser
  */
 ;(function ($, window){
@@ -31,7 +32,7 @@
                 {
                     // Unbind all events and empty the plugin data from instance
                     $(instance.element).off();
-                    $.data(this, 'rate', null);
+                    $.data(this, 'cmchooser', null);
                 }
             });
 
@@ -44,15 +45,82 @@
         this.element = element;
         this.settings = $.extend({}, $.fn.cmchooser.settings, options);
         this.build();
+        this.select(this.settings.default_colormap);
+    }
+
+    CMChooser.prototype.select = function(key)
+    {
+        var selected_colormap = {key: colorbrewer[key]};
+        var container = d3.select(this.element)
+            .select("button");
+        container.html("");
+        this.build_colormaps(container, selected_colormap);
+        this.selected = key;
+
+        /* Call change event */
+        $(this.element).trigger("change", this.selected);
     }
 
     CMChooser.prototype.build = function()
     {
-        
+        var that = this;
+        d3.select(this.element)
+            .attr("class", this.settings.dropup ? "dropup" : "dropdown")
+            .append("button")
+            .attr("class", "btn btn-default dropdown-toggle")
+            .attr("data-toggle", "dropdown");
+
+        var colormaps_container = d3.select(this.element)
+            .append("ul")
+            .attr("class", "dropdown-menu")
+            .style({
+                'width': '270px',
+                'height': '300px',
+                'overflow-y': 'scroll'
+            });
+
+        this.build_colormaps(colormaps_container, colorbrewer, function(d){
+            that.select(d.key);
+        });
+    }
+
+    CMChooser.prototype.get_selected = function()
+    {
+        return this.selected;
+    }
+
+    CMChooser.prototype.build_colormaps = function(container, colormaps, onclick)
+    {
+        container
+            .selectAll(".palette")
+            .data(d3.entries(colormaps))
+            .enter().append("li")
+            .attr("class", "palette")
+            .style({
+                'cursor':           'pointer',
+                'display':          'inline-block',
+                'vertical-align':   'bottom',
+                'background':       '#fff',
+                'border':           'solid 1px #aaa'
+            })
+            .attr("title", function(d){ return d.key; })
+            .on("click", onclick)
+            .selectAll(".swatch")
+            .data(function(d){ return d.value[d3.keys(d.value).map(Number).sort(d3.descending)[0]]; })
+            .enter().append("span")
+            .attr("class", "swatch")
+            .style({
+                'background-color': function(d) { return d; },
+                'display': 'inline-block',
+                'vertical-align': 'middle',
+                'width': '20px',
+                'height': '22px',
+            });
     }
 
     $.fn.cmchooser.settings = {
-
+        'dropup': true,
+        'default_colormap': 'PuBu'
     };
 
 }(jQuery, window));
